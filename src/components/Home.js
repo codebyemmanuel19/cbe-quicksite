ff8cb37f6d17490f7d4b649b1138681f1e6724fb
Emman091 <emmanuelkenneth063@mail.com>
2026-09-07 13:33:06 +0100

    Add bcrypt password hashing

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 function Home() {
   return (
     <section className="home">
-      <h1>Your business. Your website. Your control.</h1>
+      <h1>Get a Professional Website for Your Business, Fast.</h1>
       <p>
         Message us, tell us about your business, and get your own website
         with a dashboard set up fast, no stress.
